f89a9b956318dd54539437d0c7c0acb8e8657422
kavish sharma <kvis1611@gmail.com>
2025-04-28 17:45:34 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+document.fonts.ready.then(() => {
+  tsParticles.load("particles-js", {
+    particles: {
+      number: { value: 30 },
+      color: { value: "#ffffff" }, // Bright white
+      shape: {
+        type: "char",
+        options: {
+          char: {
+            value: [
+              "function()", 
+              "console.log()", 
+              "let i = 0;", 
+              "<div></div>", 
+              "const x = 10;", 
+              "return true;", 
+              "import React", 
+              "if (x > y)", 
+              "for(let j=0)", 
+              "=> arrow fn"
+            ],
+            font: "Pixelify Sans",
+            style: "normal",
+            weight: "200",
+          }
+        }
+      },
+      opacity: { value: 1, random: true },
+      size: { value: 14, random: true },
+      move: {
+        enable: true,
+        speed: 3,
+        direction: "none", // free/random movement
+        random: false,
+        straight: false,
+        out_mode: "bounce"
+      }
+    },
+    interactivity: {
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" }
+      },
+      modes: {
+        repulse: { distance: 100, duration: 0.4 },
+        push: { quantity: 2 }
+      }
+    },
+    retina_detect: true
+  });
+});
---
+++ assets/js/countdown.js
@@ -0,0 +1,26 @@
+var countDate = new Date('Jul 29, 2025 00:08:00').getTime();
+
+function incognitoDaysLeft(){
+    var today = new Date().getTime();
+        timeLeft = countDate - today;
+
+        var second = 1000;
+        var minute = second * 60;
+        var hour = minute * 60;
+        var day = hour * 24;
+
+        var d = Math.floor(timeLeft / (day));
+        var h = Math.floor((timeLeft % (day)) / (hour));
+        var m = Math.floor((timeLeft % (hour)) / (minute));
+        var w = Math.floor((timeLeft % (minute)) / (second));
+        
+
+        document.getElementById('day').innerHTML = d;
+        document.getElementById('hour').innerHTML = h;
+        document.getElementById('minute').innerHTML = m;
+        document.getElementById('second').innerHTML = w;
+};
+
+setInterval(function() {
+    incognitoDaysLeft();
+}, 1000);
